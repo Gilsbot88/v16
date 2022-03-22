ba5cc5589dbd9310a96844dde0338f2ed818ac99
Gilsbot88 <101207495+Gilsbot88@users.noreply.github.com>
2022-03-22 23:40:22 +0700

    Update topuppubg.js

--- plugins/topuppubg.js
+++ plugins/topuppubg.js
@@ -2,11 +2,11 @@ const topuppubg = (prefix) => {
 	return `*HALLO KAK👋*
 *PILIH DIBAWAH YA!!!*
 *LIST UC PUBG*
-*NO ILEGAL*
+*NO ILEGAL 100%*
 - 🎟️60 = Rp14.000
 - 🎟️120 = Rp28.000
 - 🎟️240 = Rp56.000
-*minat? hub :* http://wa.me/${nomorowner}
+*minat? hub :* http://wa.me/${6283136890848}
 `
 }
 
